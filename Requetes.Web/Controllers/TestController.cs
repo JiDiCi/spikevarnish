76e078b02299d6684326214b7edf949143310c05
diajul01 <diajul01@INSPQ-6399>
2018-12-17 10:49:53 -0500

    Setup

---
+++ Requetes.Web/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Requetes.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public JsonResult GetSomething()
+        {
+            return Json("dude");
+        }
+    }
+}
---
+++ Requetes.Web/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Requetes.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(options =>
+            {
+                options.WithOrigins("http://localhost:5000").AllowAnyHeader().AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+            });
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
